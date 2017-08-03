5b891c517c65938fe5751079b0cd4e703ab8e3c6
sangaizhi <sangaizhi@163.com>
2017-08-03 17:22:57 +0800

    异步执行 Job

---
+++ spring-batch-job/src/test/java/org/sangaizhi/springbatch/job/AsyncJobLaunchTest.java
@@ -0,0 +1,39 @@
+package org.sangaizhi.springbatch.job;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author sangaizhi
+ * @date 2017/8/3
+ */
+public class AsyncJobLaunchTest {
+
+    private ApplicationContext context;
+    private JobLauncher jobLauncher;
+
+    @Before
+    public void init(){
+        context = new ClassPathXmlApplicationContext("classpath:spring-batch.xml");
+        jobLauncher = (JobLauncher) context.getBean("asyncJob");
+    }
+
+    @Test
+    public void testAsyncJobLaunch(){
+        Job job = (Job) context.getBean("childJob");
+        try {
+            /* 运行Job */
+            JobExecution result = jobLauncher.run(job, new JobParameters());
+            System.out.println(result.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
